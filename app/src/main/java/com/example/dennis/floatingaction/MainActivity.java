f4f2ac09797e8e22c0ee1c60b989a2e4a1b33e3e
Deetrixx <dennisgithua30@gmail.com>
2016-05-31 18:55:16 +0300

    application for android on floating action button

---
+++ app/src/main/java/com/example/dennis/floatingaction/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dennis.floatingaction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        ListView listView = (ListView) findViewById(android.R.id.list);
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.attachToListView(listView);
+    }
+}
